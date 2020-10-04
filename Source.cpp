b617dc5330d012059503518e739b5293833b2da6
chaza fliti <chazafliti@gmail.com>
2020-10-04 23:49:49 +0300

    first commit

---
+++ Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<cmath>
+
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int y;
+
+	cout << "please enter \n";
+	cout << "a is \t";
+	cin >> a;
+	cout << "b is \t";
+	cin >> b;
+	cout << "c is \t";
+	cin >> c;
+	cout << "d is \t";
+	cin >> d;
+	y = ((((exp(a) + (3 * (log(c))) / sqrt(b ^ c)) * abs(atan(d)))));
+
+	cout << "y=" << y << endl;
+	return 0;
+}
