180ceb97a8792be47373b32645b3e68251454523
chaza fliti <chazafliti@gmail.com>
2020-10-05 15:53:54 +0300

    second commit

--- Source.cpp
+++ Source.cpp
@@ -1,26 +1,41 @@
 #include <iostream>
 #include<cmath>
+#include <fstream>
 
 
 using namespace std;
 
 int main()
 {
+	ifstream inFile;
+	inFile.open("C:\\Users\\User\\OneDrive\\Desktop\\syst.dev..txt");
+	if (!inFile) {
+		cerr << "Unable to open file datafile.txt";
+		exit(1);   // call system to stop
+	}
+	int x;
+	int i = 0;
 	int a;
 	int b;
 	int c;
 	int d;
-	int y;
+	double y;
+	while (inFile >> x) {
+		cout << x;
+		if (i == 0)
+			a = x;
+		else if (i == 1)
+			b = x;
+		else if (i == 2)
+			c = x;
+		else if (i == 3)
+			d = x;
+		i++;
 
-	cout << "please enter \n";
-	cout << "a is \t";
-	cin >> a;
-	cout << "b is \t";
-	cin >> b;
-	cout << "c is \t";
-	cin >> c;
-	cout << "d is \t";
-	cin >> d;
+	}
+	inFile.close();
+
+	
 	y = ((((exp(a) + (3 * (log(c))) / sqrt(b ^ c)) * abs(atan(d)))));
 
 	cout << "y=" << y << endl;
